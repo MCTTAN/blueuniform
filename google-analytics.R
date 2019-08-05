e19f11c48c24ac244c68208282c805121790072f
MCTTAN <mariechantaltan@gmail.com>
2019-08-05 11:33:32 +0000

    commit

--- google-analytics.R
+++ google-analytics.R
@@ -1,17 +1,11 @@
 
 require(googleAnalyticsR)
+require(googleVis)
+require(gapminder)
 
-# Authorize the Google Analytics account
-# This need not be executed in every session once the token object is created 
-# and saved
-token <- Auth("916005612054-ojq9k6t7gh6le792lv7s91e83psuss38.apps.googleusercontent.com","wnzP4cxojq6-klbBMwXmpGpV")
+head(gapminder)
+?gapminder
 
+animated_plot <- gvisMotionChart(gapminder, xvar = "pop", yvar = "lifeExp", timevar = "year", idvar = "country")
 
-query.list <- Init(start.date = "2014-09-01",
-                   end.date = "2014-11-29",
-                   dimensions = "ga:date",
-                   metrics = "ga:transactions,ga:transactionRevenue",
-                   segment = "users::sequence::^ga:userType==New%20Visitor;dateOfSession<>2014-09-01_2014-09-07;ga:campaign==Campaign%20A;->>perSession::ga:transactions>0",
-                   max.results = 10000,
-                   sort = "ga:date",
-                   table.id = tableId)
+plot(animated_plot)
